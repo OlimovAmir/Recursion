361222105706d97632011adc5876b37dbb8a592f
olimov Amir <olimov.amir@mail.ru>
2023-12-09 08:28:54 +0300

    Добавьте файлы проекта.

---
+++ Recursion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Recursion
+    {
+        static void Foo()
+        {
+            Console.WriteLine("Foo");
+            Foo();
+        }
+        static void Main(string[] args)
+        {
+            Foo();
+        }
+    }
+}
